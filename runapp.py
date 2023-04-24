d96dcc5c43d06d3ea8b2d28f0c64e91d787f80e9
Allan Ding <allan.ding@yale.edu>
2023-04-24 17:04:57 -0400

    added both hand (right hand priority)

--- runapp.py
+++ runapp.py
@@ -3,6 +3,12 @@ from flask import Flask, render_template, make_response
 app = Flask(__name__, template_folder="./templates", static_folder='./static')
 
 
+
+
+# def get_question(id):
+
+
+
 @app.route('/')
 def index():
     html = render_template(
--- static/sketch.js
+++ static/sketch.js
@@ -1,8 +1,8 @@
 
 // Adapted from https://p5js.org/examples/interaction-snake-game.html
 //
-// var host = "cpsc484-03.yale.internal:8888";
-var host = "127.0.0.1:4444";
+var host = "cpsc484-04.yale.internal:8888";
+// var host = "127.0.0.1:4444";
 
 // var host = "127.0.0.1:4444";
 
@@ -18,151 +18,170 @@ let bottomright = document.getElementById("bottomright");
 
 
 $(document).ready(function() {
-frames.start();
-//   twod.start();
+  frames.start();
 });
 
 var frames = {
-socket: null,
+  socket: null,
 
-start: function() {
-  var url = "ws://" + host + "/frames";
-  frames.socket = new WebSocket(url);
-  frames.socket.onmessage = function (event) {
-    var command = frames.get_command(JSON.parse(event.data));
-    if (command) {
-      console.log("exists", topleft);
-      if (up !== null) {
-          // if in index.html
-          // console.log("change page");
-          // change_page("question1.html")
-          check_raised_hand(command);
-          // up = null;
-          // change to hand must be risen above shoulder.
-          // sendWristCommand(command);
-      } else if (bottomright) {
-          // if in question.html
-          console.log("bruh");
-          select_square(command);
-      }
-    } else {
-      if (bottomright) {
-          resetSquares()
+  start: function() {
+    var url = "ws://" + host + "/frames";
+    frames.socket = new WebSocket(url);
+    frames.socket.onmessage = function (event) {
+      var command = frames.get_command(JSON.parse(event.data));
+      if (command) {
+        console.log("exists", topleft);
+        if (up !== null) {
+            check_raised_hand(command);
+        } else if (bottomright) {
+            // if in question.html
+            console.log("bruh");
+            select_square(command);
+        }
+      } else {
+        if (bottomright) {
+            resetSquares()
+        }
       }
     }
-  }
-},
+  },
 
-get_command: function (frame) {
-  console.log(frame)
-  var command = 0;
-  // if there are no people in the frame
-  if (frame.people.length < 1) {
+  get_command: function (frame) {
+    console.log(frame)
+    var command = 0;
+    // if there are no people in the frame
+    if (frame.people.length < 1) {
+      return command;
+    }
+
+    var pxy = frame.people[0].joints[2]["pixel"];
+    var chest_x = pxy["x"];
+    var chest_y = pxy["y"];
+    // var chest_z = pxy["y"]
+
+    var px = (frame.people[0].joints[8]["pixel"]["x"] - chest_x) * -1;
+    var py = (frame.people[0].joints[8]["pixel"]["y"] - chest_y) * -1;
+
+    console.log("normalized wrist pix",px, py);
+
+    if (py > 60) {
+      //up
+      if (px > 10) {
+        //topright
+        command = 1;
+      } else if (px < -50) {
+        //topleft
+        command = 2;
+      }
+    } else if (py < -20) {
+      if (px > 5) {
+        //bottomright
+        command = 3;
+      } else if (px < -80) {
+        //bottomleft
+        command = 4;
+      }
+    } 
+
+
+    var rpx = (frame.people[0].joints[15]["pixel"]["x"] - chest_x) * -1;
+    var rpy = (frame.people[0].joints[15]["pixel"]["y"] - chest_y) * -1;
+
+
+    console.log("right wrist pix",rpx, rpy);
+
+
+    if (rpy > 60) {
+      //up
+      if (rpx > 50) {
+        //topright
+        command = 1;
+        console.log("updated");
+      } else if (rpx < -10) {
+        //topleft
+        command = 2;
+        console.log("updated");
+      }
+    } else if (rpy < -20) {
+      if (rpx > 80) {
+        //bottomright
+        command = 3;
+        console.log("updated");
+      } else if (rpx < -5) {
+        //bottomleft
+        command = 4;
+        console.log("updated");
+
+      }
+    } 
+
+
+
+
+
+    
+
+
+
+    
+    
+
+
+    
+    // console.log(chest_x, chest_y);
+
+    // Normalize by subtracting the root (pelvis) joint coordinates
+    // var pelvis_x = frame.people[0].joints[2].position.x;
+    // var pelvis_y = frame.people[0].joints[2].position.y;
+    // var pelvis_z = frame.people[0].joints[2].position.z;
+    // var left_wrist_x = (frame.people[0].joints[7].position.x - pelvis_x) * -1;
+    // var left_wrist_y = (frame.people[0].joints[7].position.y - pelvis_y) * -1;
+    // var left_wrist_z = (frame.people[0].joints[7].position.z - pelvis_z) * -1;
+
+    // if (left_wrist_z < 100) {
+    //     return command;
+    //   }
+    
+    // if (left_wrist_y > 0) {
+    //     if (left_wrist_x > 150) {
+    //         //topright
+    //         command = 1;
+    //     } else if (left_wrist_x < -150){
+    //         //topleft
+    //         command = 2;
+    //     } else {
+    //         command = 5;
+    //     }
+    // } else if(left_wrist_y < 0) {
+    //     if (left_wrist_x > 100) {
+    //         //bottomright
+    //         command = 3;
+    //     } else if (left_wrist_x < -100){
+    //         //bottomleft
+    //         command = 4;
+    //     } else {
+    //         command = 5;
+    //     }
+    // }
+    console.log("command", command)
     return command;
-  }
+  },
 
-  var pxy = frame.people[0].joints[2]["pixel"];
-  var pelvis_x_pixel = pxy["x"];
-  var pelvis_y_pixel = pxy["y"];
-  
-  console.log(pelvis_x_pixel, pelvis_y_pixel);
 
-  // Normalize by subtracting the root (pelvis) joint coordinates
-  var pelvis_x = frame.people[0].joints[2].position.x;
-  var pelvis_y = frame.people[0].joints[2].position.y;
-  var pelvis_z = frame.people[0].joints[2].position.z;
-  var left_wrist_x = (frame.people[0].joints[7].position.x - pelvis_x) * -1;
-  var left_wrist_y = (frame.people[0].joints[7].position.y - pelvis_y) * -1;
-  var left_wrist_z = (frame.people[0].joints[7].position.z - pelvis_z) * -1;
 
-  if (left_wrist_z < 100) {
-      return command;
+  get_right_wrist_command: function (frame) {
+    var command = null;
+    if (frame.people.length < 1) {
+        return command;
     }
-  
-  if (left_wrist_y > 0) {
-      if (left_wrist_x > 150) {
-          //topright
-          command = 1;
-      } else if (left_wrist_x < -150){
-          //topleft
-          command = 2;
-      } else {
-          command = 5;
-      }
-  } else if(left_wrist_y < 0) {
-      if (left_wrist_x > 150) {
-          //bottomright
-          command = 3;
-      } else if (left_wrist_x < -150){
-          //bottomleft
-          command = 4;
-      } else {
-          command = 5;
-      }
+
+    var pelvis_x = frame.people[0].joints[0].position.x;
+    var pelvis_y = frame.people[0].joints[0].position.y;
+    var pelvis_z = frame.people[0].joints[0].position.z;
+    var right_wrist_x = (frame.people[0].joints[15].position.x - pelvis_x) * -1;
+    var right_wrist_y = (frame.people[0].joints[15].position.y - pelvis_y) * -1;
+    var right_wrist_z = (frame.people[0].joints[15].position.z - pelvis_z) * -1;
   }
-  console.log("command", command)
-  return command;
-},
-
-
-
-//   get_command_pixel: function (frame) {
-//     console.log(frame)
-//     var command = 0;
-//     // if there are no people in the frame
-//     if (frame.people.length < 1) {
-//       return command;
-//     }
-
-  // Normalize by subtracting the root (pelvis) joint coordinates
-  
-  // var pelvis_y = frame.people[0].joints[2].position.y;
-  // var pelvis_z = frame.people[0].joints[2].position.z;
-  // var left_wrist_x = (frame.people[0].joints[7].position.x - pelvis_x) * -1;
-  // var left_wrist_y = (frame.people[0].joints[7].position.y - pelvis_y) * -1;
-  // var left_wrist_z = (frame.people[0].joints[7].position.z - pelvis_z) * -1;
-
-  // if (left_wrist_z < 100) {
-  //     return command;
-  //   }
-  
-  // if (left_wrist_y > 0) {
-  //     if (left_wrist_x > 150) {
-  //         //topright
-  //         command = 1;
-  //     } else if (left_wrist_x < -150){
-  //         //topleft
-  //         command = 2;
-  //     } else {
-  //         command = 5;
-  //     }
-  // } else if(left_wrist_y < 0) {
-  //     if (left_wrist_x > 150) {
-  //         //bottomright
-  //         command = 3;
-  //     } else if (left_wrist_x < -150){
-  //         //bottomleft
-  //         command = 4;
-  //     } else {
-  //         command = 5;
-  //     }
-  // }
-  // console.log("command", command)
-  // return command;
-
-get_right_wrist_command: function (frame) {
-  var command = null;
-  if (frame.people.length < 1) {
-      return command;
-  }
-
-  var pelvis_x = frame.people[0].joints[0].position.x;
-  var pelvis_y = frame.people[0].joints[0].position.y;
-  var pelvis_z = frame.people[0].joints[0].position.z;
-  var right_wrist_x = (frame.people[0].joints[15].position.x - pelvis_x) * -1;
-  var right_wrist_y = (frame.people[0].joints[15].position.y - pelvis_y) * -1;
-  var right_wrist_z = (frame.people[0].joints[15].position.z - pelvis_z) * -1;
-}
 };
 
 
@@ -195,38 +214,12 @@ function check_answer(boxID) {
   }, 2000)
 };
 
-// let request = null;
-
-// function handleResponse(response) {
-//   console.log(response);
-//   $("body").html(response);
-// }
-
 function check_raised_hand(command) {
   if (command == 1 || command == 2) {
     let url = "/question/1";
     change_page(url);
   }
 };
-
-// function getResults()
-//          {
-//             let city = $('#cityInput').val();
-//             city = encodeURIComponent(city);
-//             let url = '/searchresults?city=' + city;
-
-//             if (request != null)
-//                request.abort();
-
-//             request = $.ajax(
-//                {
-//                   type: 'GET',
-//                   url: url,
-//                   success: handleResponse
-//                }
-//             );
-//          }
-
 
 
 
@@ -256,13 +249,6 @@ function resetSquares() {
   bottomleft.style.backgroundColor = "white";
   bottomright.style.backgroundColor = "white";
 };
-
-// function resetColors() {
-//     up.style.color = "black";
-//     down.style.color = "black";
-//     left.style.color = "black";
-//     right.style.color = "black";
-// };
 
 function sendWristCommand(command) {
   resetColors();
